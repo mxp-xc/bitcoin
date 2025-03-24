0f1b84668a1811debe4fedc919735691a9938975
mao xin chen <1010763509@qq.com>
2025-03-25 03:08:34 +0800

    添加new_kline_parser测试

---
+++ trading/script/new_kline_parser.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import asyncio
+
+from loguru import logger
+
+from conf import settings
+from trading.schema.base import KLine
+
+
+async def main():
+    symbol = "BTC/USDT:USDT"
+    timeframe = "30m"
+    async with settings.create_async_exchange() as exchange:
+        klines = [
+            KLine.from_ccxt(ohlcv)
+            for ohlcv in await exchange.watch_ohlcv(symbol, timeframe)
+        ]
+        prev_kline = klines[-1]  # 上一根k线, 刚开始为空
+
+        while True:
+            klines = [
+                KLine.from_ccxt(ohlcv)
+                for ohlcv in await exchange.watch_ohlcv(symbol, timeframe)
+            ]
+            for kline in klines:
+                if kline.opening_time > prev_kline.opening_time:
+                    closed_kline = prev_kline
+                    logger.info(f"[{symbol} - {timeframe}] 收线了. {closed_kline}")
+                prev_kline = kline
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
